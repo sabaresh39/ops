7f0a744b4597e2374126b2634a94f9f4ed9e0b83
sabaresh.velusamy <sabaresh.velusamy@ril.com>
2023-05-08 03:01:36 +0530

    feat: initial file setup

---
+++ src/app/login/page.tsx
@@ -0,0 +1,9 @@
+import { FC } from "react";
+
+interface LoginProps {}
+
+const Login: FC<LoginProps> = () => {
+  return <h1 className="text-center text-lg">Login</h1>;
+};
+
+export default Login;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,9 @@
-export default function Home() {
-  return <div className="text-red-500">Welcome to FreshMenu OMS</div>
-}
+import { FC } from "react";
+
+interface HomeProps {}
+
+const Home: FC<HomeProps> = () => {
+  return <h1 className="text-center text-lg">Welcome to FreshMenu OMS</h1>;
+};
+
+export default Home;
